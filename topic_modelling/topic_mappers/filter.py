61e3122948949cf769e6f7bf301a83930bc6d43d
LorenzoBellomo <lorenzo.bellomo.lb@gmail.com>
2022-04-22 13:31:18 +0200

    moved and new topic dumps

---
+++ topic_modelling/topic_mappers/filter.py
@@ -0,0 +1,31 @@
+import json
+
+with open("topic_dump_revised.json", 'r') as json_file:
+    rev = json.load(json_file)
+
+rev_l = list()
+for v in rev.values():
+    rev_l = rev_l + list(v)
+
+with open("topic_dump.json", 'r') as json_file:
+    dump = json.load(json_file)
+
+new_dump = {k: list() for k in dump.keys()}
+for k, v in dump.items():
+    for x in v:
+        if x not in rev_l:
+            new_dump[k].append(x)
+            rev_l.append(x)
+
+with open("topic_dump.json", 'w') as json_file:
+    json.dump(new_dump, json_file)
+
+with open("unique_500.json", 'r') as json_file:
+    unique = json.load(json_file)
+
+unique_ = []
+for x in unique:
+    if x not in rev_l:
+        unique_.append(x)
+with open("unique_500.json", 'w') as json_file:
+    json.dump(unique_, json_file)
