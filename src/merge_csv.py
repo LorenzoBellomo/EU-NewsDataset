a9d8e164584f9a3c5dd8cca911dbcbd6d7c84fc3
LorenzoBellomo <lorenzo.bellomo.lb@gmail.com>
2022-05-03 17:56:09 +0200

    small fix

--- src/merge_csv.py
+++ src/merge_csv.py
@@ -4,5 +4,8 @@ import numpy as np
 df1 = pd.read_csv("EUNewsArticles.csv", index_col=False)
 df2 = pd.read_csv("BiasClassification/data/eudata_without_url.csv", index_col=False)
 df2['url'] = np.nan
-df = pd.concat([df1, df2], ignore_index=True)
+df2.drop(columns=['date'], inplace=True)
+df1.drop(columns=['topic', 'topic2'], inplace=True)
+df2=df2[df1.columns]
+df = pd.concat([df1, df2], ignore_index=True, sort=False, axis = 0)
 df.to_csv("EUAllData.csv")
